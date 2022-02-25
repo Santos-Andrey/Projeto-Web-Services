034bf1e12f6c303652df1eeedcb74da23b714460
Andrey Santos <Andrey.profissional1821@gmail.com>
2022-02-24 22:01:16 -0300

    Implementando anotations, JPA, e Banco teste H2(que acabou retornando erro inesperado e desconhecido no H2)

--- src/main/java/com/example/demo/Entities/User.java
+++ src/main/java/com/example/demo/Entities/User.java
@@ -2,10 +2,17 @@ package com.example.demo.Entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	private String name;
 	private String email;
@@ -87,4 +94,5 @@ public class User implements Serializable {
 			return false;
 		return true;
 	}
+
 }
