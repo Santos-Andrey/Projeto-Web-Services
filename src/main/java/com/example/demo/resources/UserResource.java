0cfdc2e5f6f45121356831ff3f9b7153c582bc42
Andrey Santos <Andrey.profissional1821@gmail.com>
2022-02-24 22:34:45 -0300

    Criando a primeira camada de serviço (user) e usando componente de registro

--- src/main/java/com/example/demo/resources/UserResource.java
+++ src/main/java/com/example/demo/resources/UserResource.java
@@ -1,22 +1,33 @@
 package com.example.demo.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.Entities.User;
+import com.example.demo.services.UserServices;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+	@Autowired
+	private UserServices services;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "Maria", "Maria@gmail.com","99999999", "12345");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List> findAll(){
+		List<User> list = services.findAll();
+		return ResponseEntity.ok().body(list);
 	}
 	
-	
-	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = services.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
 }
