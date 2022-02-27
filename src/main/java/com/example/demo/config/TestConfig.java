dd3a2c15cb9b1e5dfad68ced4adce3f3d61f137f
Andrey Santos <Andrey.profissional1821@gmail.com>
2022-02-27 12:09:05 -0300

    Craindo classe Category (erro novo localizado, mas ainda desconhecido, codigos e sintaxes 100% corretas mas erro apresenta como um bug)

--- src/main/java/com/example/demo/config/TestConfig.java
+++ src/main/java/com/example/demo/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.example.demo.Entities.Category;
 import com.example.demo.Entities.Order;
 import com.example.demo.Entities.User;
 import com.example.demo.Entities.enums.OrderStatus;
+import com.example.demo.repositories.CategoryRepository;
 import com.example.demo.repositories.OrderRepository;
 import com.example.demo.repositories.UserRepository;
 
@@ -24,10 +26,17 @@ public class TestConfig implements CommandLineRunner {
 	@Autowired
 	private OrderRepository orderRepository;
 	
-	
+	@Autowired
+	private CategoryRepository categoryRepository;
 	
 	@Override
 	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category((Long)null, "Eletronics");
+		Category cat2 = new Category((Long)null, "Foods");
+		Category cat3 = new Category((Long)null, "Books");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		
 		User u1 = new User((Long) null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User((Long) null, "Alex Green", "alex@gmail.com", "977777777", "123456");
